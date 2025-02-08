7ceb1dda28719f4670ff7211e6e8d892c5fb0e53
ken-wilderotter <sophia_wilderotter@biswashington.org>
2025-02-08 21:50:52 +0000

    done

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-ptint ("hello")
+print ("hello, what is your name?")
+name = input()
+print ("hi", name, "im silly-bot! how old are you?")
+age = int(input())
+print ("how old do you think i am?")
+bot_age = int(input())
+print ("wow you got it right on the first try!")
+if age = bot_age:
+    print ("wow! i just realized, we are the same age!")
+elif age > bot_age:
+    print ("wow! i just realized, you are older than me!")
+else:
+    print ("you are younger than me! i just realized that!")
+import random
+a=random.randint(1,100)
+print ("i have a friend who is", a, "years old!")
